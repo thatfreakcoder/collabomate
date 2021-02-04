7f7d955e331146daa7b390bfea7da4014500f9c8
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2021-02-04 21:49:44 +0530

    added login, register routes

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_mysqldb import MySQL
 from os import urandom
 from yaml import load, FullLoader
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 mysql = MySQL(app)
@@ -33,21 +34,75 @@ def index():
     else:
         return jsonify({'response' : 'error', 'message': "No Database Entries Found", 'keeps': int(q)})
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        session['logged_in'] = True
-        session['user_id'] = 1
-        return redirect('/')
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        user = request.form
+        full_name = user['full_name']
+        email = user['email']
+        password = user['password']
+        linkedin = user['linkedin']
+        github = user['github']
+        skills = user['skills']
+        role = user['role']
+        links = user['links']
+        registration_number = user['registration_number']
+        batch = user['batch']
 
-@app.route('/project/<int:id>')
+        if password != user['confirmPassword']:
+            flash("Passwords do not match", "danger")
+            return render_template('register.html')
+        
+        if len(password) < 8:
+            flash("Password Length too short, Please use longer password", "danger")
+            return render_template('register.html')
+        
+        password = generate_password_hash(password)
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO profile(full_name, email, password, linkedin, github, skills, role, links, registration_number, batch) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);", (full_name, email, password, linkedin, github, skills, role, links, registration_number, batch))
+        mysql.connection.commit()
+        cur.close()
+        flash("Registration Successful !! Please Login to continue", "success")
+        return redirect('/login/')
+    return render_template('register.html')
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		userDetails = request.form
+		email_id = userDetails['email']
+		userpass = userDetails['password']
+		cur = mysql.connection.cursor()
+		usercheck = cur.execute("SELECT * FROM user WHERE email=%s", ([username]))
+		if usercheck > 0:
+			user = cur.fetchone()
+			checker = check_password_hash(user[-4], userpass)
+			print(checker)
+			if checker:
+				session['logged_in'] = True
+				session['full_name'] = user[1]
+				session['user_id'] = user[0]
+				flash(f"Welcome {session['full_name']}!! Your Login is Successful", 'success')
+			else:
+				cur.close()
+				flash('Wrong Password!! Please Check Again.', 'danger')
+				return render_template('login.html')
+		else:
+			cur.close()
+			flash('User Does Not Exist!! Please Enter Valid Username.', 'danger')
+			return render_template('login.html')
+		cur.close()
+		return redirect('/')
+	return render_template('login.html')
+
+@app.route('/project/<int:id>/')
 def project(id):
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM open_projects WHERE project_id={};".format(id))
     info = cur.fetchone()
     return render_template('project.html', info=info)
 
-@app.route('/project/new', methods=['GET', 'POST'])
+@app.route('/project/new/', methods=['GET', 'POST'])
 def new_project():
     if request.method == 'POST':
         response = request.form
@@ -64,7 +119,7 @@ def new_project():
         return redirect('/')
     return render_template('/create.html')
 
-@app.route('/profile/me')
+@app.route('/profile/me/')
 def me():
     id = session['user_id']
     cur = mysql.connection.cursor()
@@ -72,7 +127,7 @@ def me():
     q = cur.fetchone()
     return render_template('profile.html', profile=q)
 
-@app.route('/profile/<reg_no>')
+@app.route('/profile/<reg_no>/')
 def profile(reg_no):
     reg_no = str(reg_no).upper()
     cur = mysql.connection.cursor()
@@ -81,7 +136,7 @@ def profile(reg_no):
     print(q)
     return render_template('profile.html', profile=q)
 
-@app.route('/assignments')
+@app.route('/assignments/')
 def assignments():
     return None
 
